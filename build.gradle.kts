1e4db39e068f2109423afb1d4fe897f72a8a63c0
sharie.rhea <sharierhea@gmail.com>
2023-10-06 07:57:31 -0400

    Removes unused external dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,18 +6,14 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    // mavenCentral()
-    // maven {
-    //     url= uri("https://mvnrepository.com/artifact/com.tagtraum/ffsampledsp")
-    // }
+    mavenCentral()
 }
 
 dependencies {
     implementation("com.github.twitch4j:twitch4j:1.17.0")
-    implementation(group = "com.github.philippheuer.events4j", name = "events4j-handler-simple", version = "0.11.0")
-    implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.4.7")
-    // implementation("com.tagtraum:ffsampledsp:0.9.53")
-    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    implementation(group = "com.github.philippheuer.events4j", name = "events4j-handler-simple", version = "0.9.8")
+    implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.3.5")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
