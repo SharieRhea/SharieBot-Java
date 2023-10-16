bfdf6cbfcabbc11dae76fccc74dea3711901a731
sharie.rhea <sharierhea@gmail.com>
2023-10-15 21:09:17 -0400

    Implements saving user's inventory to a text file in the !shiny command.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,10 @@ repositories {
 
 dependencies {
     implementation("com.github.twitch4j:twitch4j:1.17.0")
-    implementation(group = "com.github.philippheuer.events4j", name = "events4j-handler-simple", version = "0.9.8")
-    implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.3.5")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.4.7")
+    implementation("org.xerial:sqlite-jdbc:3.43.2.0")
+    // implementation("com.tagtraum:ffsampledsp:0.9.53")
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
